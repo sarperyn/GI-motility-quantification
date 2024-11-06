8cdda1d3c9e3a5aaf80ec2ad59917c2f8144aa49
sarperyn <sarperyn@gmail.com>
2024-11-06 09:48:30 +0300

    samplewise dice implemented

--- segmentation/src/utils/metric_utils.py
+++ segmentation/src/utils/metric_utils.py
@@ -9,6 +9,17 @@ def compute_dice_score(preds, targets, smooth=1e-6):
     dice = (2. * intersection + smooth) / (union + smooth)
     return dice.item()
 
+def compute_dice_score_samplewise(preds, targets, smooth=1e-6):
+    
+    batch_size = preds.shape[0]
+    dice = 0.0
+    for i in range(batch_size):
+        intersection = (preds[i] * targets[i]).sum()
+        union = preds[i].sum() + targets[i].sum()
+        dice += (2. * intersection + smooth) / (union + smooth)
+    return (dice / batch_size).item()
+
+
 def compute_dice_score_np(preds, targets, smooth=1e-6):
 
     # Calculate Dice Score (2 * intersection / (union + smooth))
